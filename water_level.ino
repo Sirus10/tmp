336335fa1369a71c7f80968f9a7d2d4d8efba196
Sirus10 <coindre_e@hotmail.fr>
2016-04-19 11:02:23 +0200

    Update water_level.ino

--- water_level.ino
+++ water_level.ino
@@ -1,35 +1,48 @@
+/******************************************
+# This script aim at reporting tack water level
+# Site    : http://domotique.web2diz.net/
+# Detail  : http://domotique.web2diz.net/?p=596
+#
+# License : CC BY-SA 4.0
+#
+# This script use the x10rf library 
+# See source here : 
+# https://github.com/p2baron/x10rf
+#
+#
+/*******************************************/
+// including x10rf library
 #include <x10rf.h>
 
-
-#define RF_OUT 4            // OUTPUT RF 
-#define ALIM_NIVEAU_EAU_PIN 8     // Alim Capteur niveau bas
-#define NIVEAU_BAS_1_PIN 0          // CAPTEUR  NIVEAU BAS
-#define NIVEAU_BAS_2_PIN 1          // CAPTEUR  NIVEAU BA
-#define NIVEAU_BAS_3_PIN 2          // CAPTEUR  NIVEAU BA
-#define NIVEAU_BAS_4_PIN 3          // CAPTEUR  NIVEAU BA
+// define I/O PINS
+#define NIVEAU_BAS_0_PIN 0          // SENSOR LEVEL 0
+#define NIVEAU_BAS_1_PIN 1          // SENSOR LEVEL 1
+#define NIVEAU_BAS_2_PIN 2          // SENSOR LEVEL 2
+#define NIVEAU_BAS_3_PIN 3          // SENSOR LEVEL 3
+#define RF_OUT 4                    // OUTPUT RF 
 
 x10rf myx10 = x10rf(RF_OUT,0,5);
-int niveau = 0;
+int level = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-//  Serial.begin(9600);
+  // seting up the I/O PINS
+  pinMode(NIVEAU_BAS_0_PIN, INPUT);
   pinMode(NIVEAU_BAS_1_PIN, INPUT);
   pinMode(NIVEAU_BAS_2_PIN, INPUT);
   pinMode(NIVEAU_BAS_3_PIN, INPUT);
-  pinMode(NIVEAU_BAS_4_PIN, INPUT);
- //pinMode(ALIM_NIVEAU_EAU_PIN, OUTPUT);
   pinMode(RF_OUT, OUTPUT);
- digitalWrite(NIVEAU_BAS_1_PIN, HIGH); // Activation du pull-up interne
- digitalWrite(NIVEAU_BAS_2_PIN, HIGH); // Activation du pull-up interne
- digitalWrite(NIVEAU_BAS_3_PIN, HIGH); // Activation du pull-up interne
- digitalWrite(NIVEAU_BAS_4_PIN, HIGH); // Activation du pull-up interne
+  
+  // INTERNAL PULL-UP ACTIVATION 
+  digitalWrite(NIVEAU_BAS_0_PIN, HIGH); // Activation du pull-up interne
+  digitalWrite(NIVEAU_BAS_1_PIN, HIGH); // Activation du pull-up interne
+  digitalWrite(NIVEAU_BAS_2_PIN, HIGH); // Activation du pull-up interne
+  digitalWrite(NIVEAU_BAS_3_PIN, HIGH); // Activation du pull-up interne
+
   myx10.begin();          // For water level
   
 }
 
 void loop() {
-// put your main code here, to run repeatedly:
 get_and_sent_water_level();
 delay(5000);
 }
@@ -40,20 +53,21 @@ delay(5000);
       get_and_sent_water_level
 */
 int get_and_sent_water_level(){
-//digitalWrite(ALIM_NIVEAU_EAU_PIN, HIGH);
 delay(100);
-/*Serial.print(" Niveau  : ");
+/*
+Serial.print(" LEVELLEVELLEVEL  : ");
+Serial.println(digitalRead(NIVEAU_BAS_0_PIN));
+Serial.print(" LEVELLEVELLEVEL  : ");
 Serial.println(digitalRead(NIVEAU_BAS_1_PIN));
-Serial.print(" Niveau  : ");
+Serial.print(" LEVELLEVEL  : ");
 Serial.println(digitalRead(NIVEAU_BAS_2_PIN));
-Serial.print(" Niveau  : ");
-Serial.println(digitalRead(NIVEAU_BAS_3_PIN));
-Serial.print(" Niveau  : ");
-Serial.println(digitalRead(NIVEAU_BAS_4_PIN));  
+Serial.print(" LEVEL  : ");
+Serial.println(digitalRead(NIVEAU_BAS_3_PIN));  
 */
-int niveau = (1-digitalRead(NIVEAU_BAS_1_PIN)) + (1-digitalRead(NIVEAU_BAS_2_PIN)) + (1-digitalRead(NIVEAU_BAS_3_PIN)) + (1-digitalRead(NIVEAU_BAS_4_PIN)) ;
-/*Serial.print(" Niveau d'eau  : ");
-Serial.println(niveau);  */
-myx10.RFXmeter(12,0,niveau);
-//digitalWrite(ALIM_NIVEAU_EAU_PIN, HIGH);
+int level = (1-digitalRead(NIVEAU_BAS_1_PIN)) + (1-digitalRead(NIVEAU_BAS_2_PIN)) + (1-digitalRead(NIVEAU_BAS_3_PIN)) + (1-digitalRead(NIVEAU_BAS_4_PIN)) ;
+/*Serial.print(" LEVEL OF WATER  : ");
+Serial.println(level);  */
+// SENDING THE LEVEL OVER RF
+myx10.RFXmeter(12,0,level);
+
 }
